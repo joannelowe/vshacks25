3f8fa41a95ce82c12dc4ac83db484a951084fe6e
Joanne Lowe <joannejylowe@gmail.com>
2025-07-12 17:05:31 -0700

    delete

--- helloWorld.java
+++
@@ -1,9 +0,0 @@
-public class helloWorld {
-
-    public static void main(String args[]) {
-        System.out.println("HelloWorld");
-        for (int i = 0; i < 3; i++) {
-            System.out.println(i);
-        }
-    }
-}
